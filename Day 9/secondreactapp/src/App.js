7b0fb038144cbeaebffbfc1eac128a59ae846691
MOHAMMED ARSHATH A <mailforarshath@gmail.com>
2024-04-09 15:23:21 +0530

    Day 9
    Second reactt app for ASUS ROG

---
+++ Day 9/secondreactapp/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+          <code>src/App.js</code> 
+
+
+        <h1>Arshath</h1>
+        <a
+          className="App-link"
+          href="https://www.asus.com/in/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Asus india
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
